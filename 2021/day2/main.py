f82e4e63252fb726959ef902971417227c153b8e
Jacob Tilly <dev@jacobtilly.com>
2021-12-02 11:21:59 +0100

    Updated solution

--- 2021/day2/main.py
+++ 2021/day2/main.py
@@ -1,3 +1,5 @@
+from typing import Match
+
 def getInput(day,demo = False):
     if demo:
         input = open("2021/"+day+"/demo.txt", "r").read()
@@ -6,36 +8,49 @@ def getInput(day,demo = False):
     return input
 
 def createInputArray(input, separator):
-    return [{"instruction":row.split(" ")[0],"value":row.split(" ")[1]} for row in input.split(separator)]
+    return [{"instruction":row.split(" ")[0],"value":row.split(" ")[1],"delta-x":0,"delta-y":0} for row in input.split(separator)]
 
 def part1(inputArray):
     position = {"x":0,"y":0}
     for row in inputArray:
-        if row["instruction"] == "forward":
-            position["x"] += int(row["value"])
-        if row["instruction"] == "down":
-            position["y"] += int(row["value"])
-        if row["instruction"] == "up":
-            position["y"] -= int(row["value"])
+        value = int(row["value"])
+        match row["instruction"]:
+            case 'forward':
+                row["delta-x"] = value
+            case 'up':
+                row["delta-y"] = value
+            case 'down':
+                row["delta-y"] = -abs(value)
+            case _:
+                print("Okänt kommando")
+        position["x"] += row["delta-x"]
+        position["y"] += row["delta-y"]
     return position
 
 def part2(inputArray):
     position = {"x":0,"y":0,"aim":0}
     for row in inputArray:
-        if row["instruction"] == "forward":
-            position["x"] += int(row["value"])
-            position["y"] += int(row["value"]) * position["aim"]
-        if row["instruction"] == "down":
-            position["aim"] += int(row["value"])
-        if row["instruction"] == "up":
-            position["aim"] -= int(row["value"])
+        value = int(row["value"])
+        match row["instruction"]:
+            case 'forward':
+                row["delta-x"] = value
+                row["delta-y"] = value * position["aim"]
+            case 'up':
+                position["aim"] += value
+            case 'down':
+                position["aim"] -= value
+            case _:
+                print("Okänt kommando")
+        position["x"] += row["delta-x"]
+        position["y"] += row["delta-y"]
+
     return position
 
 # Execute code below:
 # Part 1
 position = part1(createInputArray(getInput("day2"),"\n"))
-print(int(position["x"])*int(position["y"]))
+print(int(position["x"])*abs(int(position["y"])))
 
 # Part 2
 position = part2(createInputArray(getInput("day2"),"\n"))
-print(int(position["x"])*int(position["y"]))
+print(int(position["x"])*abs(int(position["y"])))
