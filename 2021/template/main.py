a61674c5d37c255adadc45667ae77e7da175a324
Jacob Tilly <dev@jacobtilly.com>
2022-01-06 20:44:24 +0100

    Prepared day 6 and updated template

--- 2021/template/main.py
+++ 2021/template/main.py
@@ -1,3 +1,5 @@
+import time
+
 def getInput(day,demo = False):
     if demo:
         input = open("2021/"+day+"/demo.txt", "r").read()
@@ -10,8 +12,12 @@ def createInputArray(input, separator):
     # Modifiera denna rad för att skapa data som makear sense
 
 def part1(inputArray):
-
+    return
 def part2(inputArray):
-    
+    return
 
 # Execute code below:
+starttime = time.time()
+
+
+print("Execution time: %s seconds" % (round(time.time()-starttime, 5)))
