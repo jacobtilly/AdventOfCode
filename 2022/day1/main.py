d41ad21e5e5096fdaa7782f96e0cbb6411a439a6
Jacob Tilly <dev@jacobtilly.com>
2022-12-06 17:03:13 +0100

    Init 2022, solution day1

---
+++ 2022/day1/main.py
@@ -0,0 +1,22 @@
+input = open("2022/day1/input.txt", "r").read()
+
+inputArray = [ x for x in input.split("\n") ]
+resultArray, currentSum = [], 0  # init
+
+for item in inputArray:
+    if item != "":
+        currentSum += int(item)
+    else:
+        resultArray.append(currentSum)
+        currentSum = 0
+
+print("*"*20)
+print("Day One")
+print("Part One Answer: "+str(max(resultArray)))
+
+resultArray.sort()
+part2Array = resultArray[-3:]
+part2Answr = sum(part2Array)
+
+print("Part Two Answer: "+str(part2Answr))
+print("*"*20)
